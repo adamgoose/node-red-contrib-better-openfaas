2556499e5ccb9aeba83192378a87d1067b379bc3
Adam Engebretson <adam@bridgeconx.com>
2019-01-10 21:59:52 -0600

    Initial Commit

---
+++ openfaas.js
@@ -0,0 +1,35 @@
+module.exports = function (RED) {
+  var axios = require('axios')
+
+  function OpenFaaSServerNode (config) {
+    RED.nodes.createNode(this, config)
+    this.endpoint = config.endpoint
+  }
+  RED.nodes.registerType('openfaas-server', OpenFaaSServerNode)
+
+  function OpenFaaSInvokeNode (config) {
+    RED.nodes.createNode(this, config)
+    this.serverConfig = RED.nodes.getNode(config.server)
+
+    if (!this.serverConfig) {
+      this.error("invalid server config")
+    }
+
+    const client = axios.create({
+      baseURL: this.serverConfig.endpoint + "/function/" + config.name
+    })
+
+    this.on('input', msg => {
+      client.post('/', msg.payload)
+        .then(r => {
+          msg.requestPayload = msg.payload
+          msg.payload = r.data
+          this.send(msg)
+        }, e => {
+          this.error('unable to invoke func: ' + e.response.data)
+        })
+    })
+
+  }
+  RED.nodes.registerType('openfaas-invoke', OpenFaaSInvokeNode)
+}
